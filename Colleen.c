49ded29064af280e1ed806ece1aa7ac43a6e94ed
Tylerdurden230 <75187191+TylerDurden230@users.noreply.github.com>
2024-10-26 12:58:02 +0200

    Add files via upload

--- Colleen.c
+++ Colleen.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-// Commento fuori dalla funzione main
+// Outside comment
 
 void helper_function() {
-    char *source = "#include <stdio.h>%c%c// Commento fuori dalla funzione main%c%cvoid helper_function() {%c    char *source = %c%s%c;%c    printf(source, 10, 10, 10, 10, 10, 34, source, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10);%c}%c%cint main() {%c    // Commento all'interno della funzione main%c    helper_function();%c    return 0;%c}%c";
+    char *source = "#include <stdio.h>%c%c// Outside comment%c%cvoid helper_function() {%c    char *source = %c%s%c;%c    printf(source, 10, 10, 10, 10, 10, 34, source, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10);%c}%c%cint main() {%c    // Inside comment%c    helper_function();%c    return 0;%c}%c";
     printf(source, 10, 10, 10, 10, 10, 34, source, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10);
 }
 
 int main() {
-    // Commento all'interno della funzione main
+    // Inside comment
     helper_function();
     return 0;
 }
