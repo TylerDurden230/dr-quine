f59ed43c2cf4d2624dbb9e829100ba5739f8afb6
Tylerdurden230 <75187191+TylerDurden230@users.noreply.github.com>
2024-10-26 12:52:13 +0200

    it works, makefile test rule added

--- Colleen.c
+++ Colleen.c
@@ -3,8 +3,8 @@
 // Commento fuori dalla funzione main
 
 void helper_function() {
-    char *source = "#include <stdio.h>%c%c// Commento fuori dalla funzione main%c%cvoid helper_function() {%c    char *source = %c%s%c;%c    printf(source, 10, 10, 10, 10, 10, 34, source, 34, 10, 10, 10, 10, 10, 10, 10, 10);%c}%c%cint main() {%c    // Commento all'interno della funzione main%c    helper_function();%c    return 0;%c}%c";
-    printf(source, 10, 10, 10, 10, 10, 34, source, 34, 10, 10, 10, 10, 10, 10, 10, 10);
+    char *source = "#include <stdio.h>%c%c// Commento fuori dalla funzione main%c%cvoid helper_function() {%c    char *source = %c%s%c;%c    printf(source, 10, 10, 10, 10, 10, 34, source, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10);%c}%c%cint main() {%c    // Commento all'interno della funzione main%c    helper_function();%c    return 0;%c}%c";
+    printf(source, 10, 10, 10, 10, 10, 34, source, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10);
 }
 
 int main() {
--- Makefile
+++ Makefile
@@ -13,5 +13,14 @@ clean:
 
 fclean: clean
 	rm -f $(NAME)
+	if [ -f tmp_Colleen ]; then rm -f tmp_Colleen; fi
 
 re: fclean all
+
+test: all
+	@echo "Colleen test:"
+	./Colleen > tmp_Colleen
+	diff Colleen.c tmp_Colleen
+	if [ $$? -eq 0 ]; then echo "OK"; else echo "KO"; fi
+
+.PHONY: all clean fclean re test
